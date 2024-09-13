f4295ab2a912236a23f2d69c921729dcc1d61e41
harshal0151 <patilharshal0151@gmail.com>
2024-09-13 14:36:47 +0530

    udate a code

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -72,6 +72,7 @@ const Contact = () => {
                 name="name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
+                required
               />
               <input
                 type="text"
@@ -80,6 +81,7 @@ const Contact = () => {
                 name="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                required
               />
             </div>
             <input
@@ -89,6 +91,7 @@ const Contact = () => {
               name="subject"
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
+              required
             />
             <textarea
               placeholder="Message"
@@ -96,6 +99,7 @@ const Contact = () => {
               name="message"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
+              required
             ></textarea>
 
             <button
